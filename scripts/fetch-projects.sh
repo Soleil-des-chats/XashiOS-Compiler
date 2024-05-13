52ffac3ea1f1cec5f297e8430e65d68d6f41e8e1
Théodore Jeanne <61901116+Soleil-des-chats@users.noreply.github.com>
2024-05-13 21:09:57 +0200

    Update fetch-projects.sh

--- scripts/fetch-projects.sh
+++ scripts/fetch-projects.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone https://github.com/FWGS/xash3d --depth 1
+git clone https://github.com/Soleil-des-chats/Xash3D-iOS --depth 1
 git clone https://github.com/FWGS/nanogl --depth 1
 git clone https://github.com/FWGS/microndk --depth 1
 wget http://libsdl.org/release/SDL2-2.0.5.tar.gz
